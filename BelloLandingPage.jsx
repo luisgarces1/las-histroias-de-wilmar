f1062f2f56fa31db98ba91e94e4031e07ac8f727
Luis Gabriel Garces Torres <luis.garces@bello.local>
2026-07-15 10:18:36 -0500

    reorder stories: set talleres del ferrocarril.mp4 as first story, huelga de bello as second

--- BelloLandingPage.jsx
+++ BelloLandingPage.jsx
@@ -67,6 +67,7 @@ const PLACES_DATA = [
 
 // Lista de Videos de "Las Historias de Wilmar" (Aleatorios)
 const WILMAR_STORIES_LIST = [
+  "/assets/video/talleres del ferrocarril.mp4",
   "/assets/video/La_Huelga_de_Bello_de_1920 ampliado.mp4",
   "/assets/video/La_huelga_de_Bello_de_1920.mp4"
 ];
